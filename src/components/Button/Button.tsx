b41bd0db0ef5cf0fc6985491e1d6cdc7ceace344
ktcotz <naskret.kamil@gmail.com>
2024-11-11 14:01:34 +0100

    feat: setup button component
    Work around button component and setup stories.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,3 +1,12 @@
-export const Button = () => {
-  return <button>Button</button>;
+import { HTMLAttributes, ReactNode } from "react";
+
+type ButtonProps = {
+  children: ReactNode;
+};
+
+export const Button = ({
+  children,
+  ...rest
+}: ButtonProps & HTMLAttributes<HTMLButtonElement>) => {
+  return <button {...rest}>{children}</button>;
 };
