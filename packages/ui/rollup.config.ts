8c62b4b60f8e397f3fbed33971ede404e12a69b7
ktcotz <naskret.kamil@gmail.com>
2025-08-04 21:32:12 +0200

    feat: rollup bundling
    Improve some of rollup bundling tips.

--- packages/ui/rollup.config.ts
+++ packages/ui/rollup.config.ts
@@ -4,36 +4,36 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import alias from '@rollup/plugin-alias';
 import * as path from 'path';
-
+import json from '@rollup/plugin-json';
 import tailwindcssPostcss from '@tailwindcss/postcss';
 import autoprefixer from 'autoprefixer';
+import { terser } from 'rollup-plugin-terser';
 
 const extensions = ['.ts', '.tsx', '.js', '.jsx'];
 
 export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: 'dist/index.js',
-      format: 'esm',
-      sourcemap: true,
-    },
-    {
-      file: 'dist/index.cjs',
-      format: 'cjs',
-      sourcemap: true,
-    },
-  ],
-  external: ['react', 'react-dom', 'clsx'],
+  input: {
+    index: 'src/index.ts',
+    cli: 'src/bin/cli.ts',
+  },
+  output: {
+    dir: 'dist',
+    format: 'esm',
+    entryFileNames: '[name].js',
+  },
+  external: ['react', 'react-dom', 'clsx', 'inquirer', 'node-fetch', 'fs', 'path'],
   plugins: [
     alias({
       entries: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
     }),
     resolve({ extensions }),
+    json(),
+
     commonjs(),
     postcss({
       extract: 'styles.css',
       minimize: true,
+      sourceMap: false,
       plugins: [tailwindcssPostcss(), autoprefixer()],
     }),
     typescript({
@@ -41,5 +41,6 @@ export default {
       useTsconfigDeclarationDir: true,
       clean: true,
     }),
+    terser(),
   ],
 };
