4a02029fd4dc71098156559ae2c60245b0c78ba0
ktcotz <naskret.kamil@gmail.com>
2025-08-04 21:04:17 +0200

    feat: rollup
    Switch building ecosystem to rollup, improve performance and
    bundle size.

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import "vive-ui/dist/styles.css";
 import "./globals.css";
-import "@vive/ui/dist/styles.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
